8306a5fc632f0f4325d9f8aef4060f2bf11d6594
Nicolas Giraud <ng.software.dev@gmail.com>
2022-01-04 10:03:57 +0100

    RetroArch - PSX

--- RApp/backup/mame2003_plus/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo $0 $*
-progdir=`dirname "$0"`
-
-./cpufreq.sh
-cd $RA_DIR/
-HOME=/mnt/SDCARD/RetroArch/ $progdir/../../RetroArch/retroarch -v -L $progdir/mame2003_xtreme_libretro.so "$1"
