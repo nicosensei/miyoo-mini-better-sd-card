885508466b19cdd20345fab28876f0e722a2fa47
Nicolas Giraud <ng.software.dev@gmail.com>
2022-01-04 10:25:42 +0100

    More cores

--- RApp/backup/easyrpg/launch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo $0 $*
-progdir=`dirname "$0"`
-
-cd $RA_DIR/
-HOME=/mnt/SDCARD/RetroArch/ $progdir/../../RetroArch/retroarch -v -L $progdir/easyrpg_libretro.so "$1"
---
+++ RApp/backup/nintendo_gba_vba_next/launch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo $0 $*
+RA_DIR=/mnt/SDCARD/RetroArch
+
+./cpufreq.sh
+cd $RA_DIR/
+HOME=$RA_DIR/ $RA_DIR/retroarch -v -L $RA_DIR/.retroarch/cores/vba_next_libretro.so "$1"
