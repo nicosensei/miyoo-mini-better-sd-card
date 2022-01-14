7641c9db511c9705b1e0406f37b8435bbcd789b7
Nicolas Giraud <ng.software.dev@gmail.com>
2022-01-14 23:36:31 +0100

    Reminiscence data

--- App/Reminiscence/launch.sh
+++ App/Reminiscence/launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/mnt/SDCARD/RetroArch/launch_core_rom.sh reminiscence /mnt/SDCARD/roms/reminiscence/data/level1.map
+/mnt/SDCARD/RetroArch/launch_core_rom.sh reminiscence /mnt/SDCARD/roms/flashback/LEVEL1.MAP
