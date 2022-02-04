2aa013c3b1135cc6178cc910714d0fd7e7848131
Nicolas Giraud <ng.software.dev@gmail.com>
2022-02-04 16:52:02 +0100

    Uppercase path / hard-coded ones expected by the frontend

--- App/Cannonball/launch.sh
+++ App/Cannonball/launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/mnt/SDCARD/RetroArch/launch_core_rom.sh cannonball /mnt/SDCARD/Roms/cannonball/CannonBall.game performance
+/mnt/SDCARD/RetroArch/launch_core_rom.sh cannonball /mnt/SDCARD/Roms/CANNONBALL/CannonBall.game performance
